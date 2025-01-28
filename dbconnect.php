5c957a72100d7429f482fcf1c96f9076eb8f323f
beemcraf <pomsanbeem9@gmail.com>
2025-01-28 10:50:41 +0700

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,6 @@
+<!--<link href="css/style.css" rel="stylesheet">-->
+<?php
+$con = mysqli_connect("localhost", "its66040233103", "K9qlX1L3", "its66040233103") or die("เกิดข้อผิดพลาดเกิดขึ้น");
+
+mysqli_set_charset($con, "utf8");
+?>
